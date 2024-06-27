58f76633889625300f85c0394844983c02fa22ad
komiko44240BB <komikobattlebay@gmail.com>
2024-06-27 21:32:31 +0200

    è

---
+++ cmake-build-debug/CMakeFiles/BattleBay_tool.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BattleBay_tool.exe"
+  "BattleBay_tool.exe.manifest"
+  "BattleBay_tool.pdb"
+  "CMakeFiles/BattleBay_tool.dir/main.c.obj"
+  "CMakeFiles/BattleBay_tool.dir/main.c.obj.d"
+  "libBattleBay_tool.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BattleBay_tool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
