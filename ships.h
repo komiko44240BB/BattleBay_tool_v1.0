851ab0bd8b18b9e09031a68b5023e6c3816129bd
komiko44240BB <komikobattlebay@gmail.com>
2024-07-24 21:04:21 +0200

    f

--- ships.h
+++ ships.h
@@ -1,27 +1,15 @@
 #ifndef SHIPS_H
 #define SHIPS_H
 
-
-
-
 struct Ship{
     struct Objects** loadout [8];
     float speed;
     float ship_agility;
     float turet_agility;
-    int gear_score;
-    int hit_points;
-    int defence;
-    int num_red;
-    int max_red;
-    int num_blue;
-    int max_blue;
-    int num_yellow;
-    int max_yellow;
-    int num_green;
-    int max_green;
-    int num_teal;
-    int max_teal;
+    unsigned int gear_score;
+    unsigned int hit_points;
+    unsigned int defence;
+    unsigned int gear [5][4];
 };
 
 #endif //SHIPS_H
