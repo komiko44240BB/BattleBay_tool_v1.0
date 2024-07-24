9993386ba50cc2c6aeed7b0016394cb1707ab2a4
komiko44240BB <komikobattlebay@gmail.com>
2024-07-24 22:01:39 +0200

    Delete cmake-build-debug directory

--- cmake-build-debug/CMakeFiles/BattleBay_tool.dir/objects.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/BattleBay_tool.dir/objects.c.o: \
- /mnt/c/Users/Pierre-Luc/Documents/GitHub/BattleBay_tool/objects.c \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/Pierre-Luc/Documents/GitHub/BattleBay_tool/objects.h
--- cmake-build-debug/CMakeFiles/BattleBay_tool.dir/ships.c.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/BattleBay_tool.dir/ships.c.o: \
- /mnt/c/Users/Pierre-Luc/Documents/GitHub/BattleBay_tool/ships.c \
- /usr/include/stdc-predef.h \
- /mnt/c/Users/Pierre-Luc/Documents/GitHub/BattleBay_tool/ships.h
