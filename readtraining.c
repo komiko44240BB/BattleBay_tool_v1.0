9f68c17d8ed14eb31c276947b030cc6af6b151b0
komiko44240BB <komikobattlebay@gmail.com>
2024-09-10 21:24:13 +0200

    Update readtraining.c

--- readtraining.c
+++ readtraining.c
@@ -1,6 +1,11 @@
 #include "readtraining.h"
 
+#include <stddef.h>
+
 
 void readAndApplyTraining(struct Object* o,char* training_file_name, unsigned int training_lvl) {
-
+    if(o == NULL || training_file_name == NULL || training_lvl == 0){
+        return;
+    }
+    
 }
